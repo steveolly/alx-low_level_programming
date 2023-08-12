fecc2c464818abbac4af65484bfff0b8c037c498
steveolly <ajibolastephen1@gmail.com>
2023-08-12 06:22:14 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char C;
+
+	for (C = 'a'; C <= 'z'; C += 1)
+	{
+		putchar(C);
+	}
+return (0);
+}
