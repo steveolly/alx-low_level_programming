baab4cc113a64643ed801f8d5cd65a152bc4ee9b
steveolly <ajibolastephen1@gmail.com>
2023-08-12 07:04:01 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,15 @@
 int main(void)
 {
 	char C;
+	char B;
 
 	for (C = 'a'; C <= 'z'; C += 1)
 	{
 		putchar(C);
+	}
+	for (B = 'A'; B <= 'Z'; B += 1)
+	{
+		putchar(B);
 	}
 putchar('\n');
 return (0);
