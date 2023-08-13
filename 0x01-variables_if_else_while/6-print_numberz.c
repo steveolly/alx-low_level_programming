82c19876f993b80eb4b3cb94d1b4571fa36fda3a
steveolly <ajibolastephen1@gmail.com>
2023-08-13 12:15:23 -0700

    Numberz as strings

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the function main
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+putchar('\n');
+return (0);
+}
