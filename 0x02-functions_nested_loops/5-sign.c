0abb2d1141948ff3531bf0c63e39f8e360258e3b
steveolly <ajibolastephen1@gmail.com>
2023-08-16 14:37:54 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - the function name
+ * @n: the variable initiated
+ * Return: return something
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
