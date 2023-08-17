57d9510f0c9b4e3fe342f5dc807fa73132464b67
steveolly <ajibolastephen1@gmail.com>
2023-08-16 17:15:52 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function name
+ * @a: interger name 1
+ * @b: interger name 2
+ * Return: return something
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
