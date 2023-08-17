e40dcfa05114ca9f10dd16545510818c61f0ce02
steveolly <ajibolastephen1@gmail.com>
2023-08-16 17:50:56 -0700

    Counting of numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function name
+ * @n: number
+ * Return: noting
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("\n");
+}
