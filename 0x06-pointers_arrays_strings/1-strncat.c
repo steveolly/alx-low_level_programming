affa73d06b774d417b1d5f2df681e10dde869e9b
steveolly <ajibolastephen1@gmail.com>
2023-08-23 16:06:19 -0700

    string cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - function
+ * @dest: var1
+ * @src: var2
+ * @n: var3
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		destlen++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[destlen + i] = src[i];
+	}
+	dest[destlen + n] = '\0';
+	return (dest);
+}
