faf6d64de5cb7e128b75c1fae8c4f32813751e6e
steveolly <ajibolastephen1@gmail.com>
2023-09-05 13:01:50 -0700

    duplicate with malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - function name of another malloc question
+ * @str: Variable for the string parameter
+ *
+ * Return: a pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *string;
+	int i;
+	int count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count ++;
+
+	if (str == NULL)
+		return (NULL);
+
+	string = malloc(sizeof(char) * count);
+
+	if (string == NULL)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+		string[i] = str[i];
+
+	return (string);
+	free(string);
+}
