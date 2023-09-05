d42ef4263e212dd0263815c13bb1e5906f7d9606
steveolly <ajibolastephen1@gmail.com>
2023-09-05 13:11:41 -0700

    duplicate with malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	string = malloc(sizeof(char) * count);
+	string = malloc(sizeof(char) * (count + 1));
 
 	if (string == NULL)
 		return (NULL);
