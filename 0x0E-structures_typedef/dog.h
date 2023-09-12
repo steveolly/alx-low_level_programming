87829b2c8893828cce67a4c10846861b1b13fee1
steveolly <ajibolastephen1@gmail.com>
2023-09-12 16:27:03 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
-	char* name;
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 #endif
