18b0c3ae15ae9557c81b0cc88a0d386df8b5fd7a
steveolly <ajibolastephen1@gmail.com>
2023-10-12 17:39:11 -0700

    digit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - ...
+ *
+ * Return: ..
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		int mul;
+
+		mul = 3 * i;
+		if (mul < 1024)
+			sum = sum + mul;
+	}
+	for (i = 0; i < 1024; i++)
+	{
+		int mul;
+
+		mul = 5 * i;
+		if (mul < 1024)
+			sum = sum + mul;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
