45b73bc1626f73d97ee17fbd4b02174126511d14
steveolly <ajibolastephen1@gmail.com>
2023-10-12 17:07:49 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,10 +21,10 @@ void print_times_table(int n)
 					printf("%d,", result);
 				else
 				{
-					printf("%3d", result);
+					printf("%4d", result);
 
 					if (j != n)
-						printf(" , ");
+						printf(",");
 				}
 			}
 			printf("\n");
