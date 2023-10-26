9572961eb5aa4b0e8856536cd7d85dc5287bdbe3
steveolly <ajibolastephen1@gmail.com>
2023-10-26 16:16:06 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,11 +18,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
 		answer = answer * 2;
-	
 		if (b[i] == '1')
 			answer++;
 	}
-	return (answer);
+return (answer);
 }
