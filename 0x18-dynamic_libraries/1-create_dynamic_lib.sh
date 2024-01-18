b09d6393ec8adf515836c681546453ac2444fb60
steveolly <ajibolastephen1@gmail.com>
2024-01-18 12:41:45 -0800

    task2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c -fPIC *.c
-gcc -shared -o liballl.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -c *.c -fpic && gcc *o -shared -o liball.so
